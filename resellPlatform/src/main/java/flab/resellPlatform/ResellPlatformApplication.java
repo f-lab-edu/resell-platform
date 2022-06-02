5711a5ff484eb850c52490b1da2640daccada2b4
michael <mskim_451@dongguk.edu>
2022-06-02 21:02:54 +0900

    Spring setup
    [Setting]
    Gradle
    Java 11
    Spring Boot 2.7.0
    
    [Library]
    spring Web, lombok

---
+++ resellPlatform/src/main/java/flab/resellPlatform/ResellPlatformApplication.java
@@ -0,0 +1,13 @@
+package flab.resellPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResellPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResellPlatformApplication.class, args);
+	}
+
+}
