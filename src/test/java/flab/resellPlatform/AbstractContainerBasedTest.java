c356fef886806e254d87a27ec53ed0d21c15c4ed
michael <mskim_451@dongguk.edu>
2022-07-22 17:36:11 +0900

    feature : Testcontainer 구현 주석 추가
    1. Commit f4870ee의 커밋 메세지에 잘못된 내용을 올렸기에 수정 커밋 겸 이해를 돕기 위한 주석을 추가함.
    
    2. 도커 컨테이너는 local에 포트를 직접적으로 노출하는게 아닌, localhost:randomPort와 도커 컨테이너의 exposed 포트가 매핑되는 형태로 타 도커 컨테이너와 소통함. 타 도커 컨테이너는 localhost:randomPort를 이용하여 target 도커 컨테이너와 소통함.
    
    3. 자세한 내용은 주석 참조 요망.

--- src/test/java/flab/resellPlatform/AbstractContainerBasedTest.java
+++ src/test/java/flab/resellPlatform/AbstractContainerBasedTest.java
@@ -22,6 +22,17 @@ public abstract class AbstractContainerBasedTest {
                 new ImageFromDockerfile()
                         .withFileFromClasspath("Dockerfile", CACHE_PATH + "Dockerfile"))
                 .withReuse(true)
+                /*
+                * 기능 : 도커 컨테이너가 localhost:randomPort와 매핑될 포트를 선정
+                *
+                * 도커 컨테이너가 올라가서 외부에 Exposed 될 포트를 선정해야함.
+                * 이를 선정하지 않으면 Testcontainer가 어떤 포트로 localhost:randomPort와 매핑시킬지 알 수가 없음.
+                * 그 결과 해당 도커 컨테이너는 외부와 고립된채로 띄워짐. (withExposedPort를 사용하지 않으면 도커 컨테이너가 외부와 고립된다는 뜻과 동일)
+                *
+                * docker-compose는 작성된 도커 컨테이너들을 하나의 네트워크로 자동으로 묶어주지만, 현재는 docker-compose를 사용하지 않기 때문에
+                * 도커 컨테이너를 어디서든 접근할 수 있게 만들어야함. 그래야 spring이 접근할 수 있기 때문. localhost:randomPort와 서버의 기본 포트를 묶어주면
+                * 해당 도커 컨테이너는 localhost:randomPort로 접근할 수 있고, 이에 spring 또한 접근할 수 있게됨.
+                 */
                 .withExposedPorts(REDIS_DEFAULT_PORT);
         REDIS_CACHE_CONTAINER.start();
 
@@ -46,6 +57,14 @@ public abstract class AbstractContainerBasedTest {
 
     @DynamicPropertySource
     public static void overrideProperties(DynamicPropertyRegistry registry) {
+        /*
+        * Q. How DynamicPropertySource works?
+        *
+        * 해당 클래스를 Override 받을 클래스는 통합 테스트를 진행할 클래스임. JVM 실행 순서 상 sub 클래스가 실행되기 전에
+        * 먼저 super 클래스가 실행됨. 따라서 스프링 컨테이너가 만들어지기 전에 AbstractContainerBasedTest가 실행되고,
+        * 이에 application.properties가 동적으로 오버라이딩됨. 이후 sub 클래스인 통합 테스트를 진행할 클래스가 만들어짐.
+        * 이때 스프링컨테이너가 만들어질때 이전에 이미 동적으로 오버라이딩 된 application.properties를 사용함.
+         */
         registry.add("spring.session.redis.master.host", REDIS_SESSION_CONTAINER::getHost);
         registry.add("spring.session.redis.master.port", () -> "" + REDIS_SESSION_CONTAINER.getMappedPort(REDIS_DEFAULT_PORT));
 
