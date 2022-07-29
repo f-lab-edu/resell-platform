a50d78f8acd52c9fd45a370e074c84b7cf2c41f3
jvnlee <lhjun1028@naver.com>
2022-07-29 17:19:01 +0900

    feat: 유저 정보 캐싱용 Redis 저장소 추가
    RedisCacheConfig를 추가하여 새로운 캐싱용 저장소를 세팅함. 기존 Redis 저장소는 Refresh Token을 저장하는 역할을 하고, 새로 만든 Redis 저장소는 DB에서 조회한 유저 정보를 username-user data 쌍으로 캐싱하여 불필요한 DB 접근을 줄여주는 기능을 함.

--- src/main/java/flab/resellPlatform/ResellPlatformApplication.java
+++ src/main/java/flab/resellPlatform/ResellPlatformApplication.java
@@ -2,8 +2,10 @@ package flab.resellPlatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ResellPlatformApplication {
 
 	public static void main(String[] args) {
--- src/main/java/flab/resellPlatform/repository/UserRepository.java
+++ src/main/java/flab/resellPlatform/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package flab.resellPlatform.repository;
 
 import flab.resellPlatform.domain.User;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.Optional;
 
@@ -9,6 +10,8 @@ public interface UserRepository {
     User save(User user);
     User update(Long id, User user);
     Optional<User> findById(Long id);
+
+    @Cacheable(key = "#username", value = "user")
     Optional<User> findByUsername(String username);
 
 }
