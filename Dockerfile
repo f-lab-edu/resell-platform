c72519da5e5767ff253f9d7a52f251d4aaedcf5f
michael <mskim_451@dongguk.edu>
2022-08-29 16:43:35 +0900

    devops : 배포 자동화
    배포 자동화

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM adoptopenjdk/openjdk11:ubi
 ARG JAR_FILE=build/libs/*.jar
 COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java", "-jar", "/app.jar"]
+ENTRYPOINT ["java", "-jar", \
+"-Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-jwt.properties,/home/ec2-user/app/application-real-db.properties", \
+"/app.jar", \
+"2>&1 &"]
---
+++ scripts/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step1
+PROJECT_NAME=resell-platform
+
+cd $REPOSITORY/$PROJECT_NAME/
+
+echo "> Git Pull"
+
+git pull
+
+echo "> 프로젝트 Build 시작"
+
+./gradlew build
+
+echo "> step1 디렉토리로 이동"
+
+cd $REPOSITORY
+
+echo "> Build 파일복사"
+
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+
+CURRENT_DOCKER_ID=$(docker ps -af "ancestor=resell-platform_app" -q)
+echo ">현재 애플리케이션 docker id: $CURRENT_DOCKER_ID"
+
+echo "stop and remove containers"
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+sleep 5
+
+echo "> 새 애플리케이션 배포"
+docker-compose -f $PROJECT_NAME/docker-compose.yml -f $PROJECT_NAME/docker-compose.prod.yml up -d
+
+NEW_DOCKER_ID=$(docker ps -af "ancestor=resell-platform_app" -q)
+echo ">새 애플리케이션 docker id: $NEW_DOCKER_ID"
