5056e2472f1262bebfde2cb71cce4df62dc828d2
michael <mskim_451@dongguk.edu>
2022-08-29 17:40:14 +0900

    devops : deploy 실행 권한 변경 in CI(gradle.yml)
    deploy 실행 권한 변경 in CI(gradle.yml)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,13 +13,9 @@ echo "> 프로젝트 Build 시작"
 
 ./gradlew build
 
-echo "> step1 디렉토리로 이동"
+echo "> step1/zip 디렉토리로 이동"
 
-cd $REPOSITORY
-
-echo "> Build 파일복사"
-
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+cd $REPOSITORY/zip
 
 CURRENT_DOCKER_ID=$(docker ps -af "ancestor=resell-platform_app" -q)
 echo ">현재 애플리케이션 docker id: $CURRENT_DOCKER_ID"
@@ -30,7 +26,7 @@ docker rm $(docker ps -a -q)
 sleep 5
 
 echo "> 새 애플리케이션 배포"
-docker-compose -f $PROJECT_NAME/zip/docker-compose.yml -f $PROJECT_NAME/zip/docker-compose.prod.yml up -d
+docker-compose -f $PROJECT_NAME/docker-compose.yml -f $PROJECT_NAME/docker-compose.prod.yml up -d
 
 NEW_DOCKER_ID=$(docker ps -af "ancestor=resell-platform_app" -q)
 echo ">새 애플리케이션 docker id: $NEW_DOCKER_ID"
