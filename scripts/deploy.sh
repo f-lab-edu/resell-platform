8f6d3ea5266fab78caf35fb775b2b63a8372c8f3
michael <mskim_451@dongguk.edu>
2022-09-05 09:26:32 +0900

    devops : 새 애플리케이션 중단 배포
    1. 새 애플리케이션 중단 배포

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,20 +5,9 @@ PROJECT_NAME=resell-platform
 
 cd $REPOSITORY/$PROJECT_NAME/
 
-#echo "> Change gradlew to executable"
-
-#chmod +x gradlew
-
-#echo "> 프로젝트 Build 시작"
-
-#./gradlew build
-
-echo "> step1/zip 디렉토리로 이동"
+echo "> 프로젝트 디렉토리로 이동"
 
 cd $REPOSITORY/$PROJECT_NAME
-
-CURRENT_DOCKER_ID=$(docker ps -af "ancestor=*app" -q)
-echo ">현재 애플리케이션 docker id: $CURRENT_DOCKER_ID"
 
 echo "stop and remove containers"
 docker stop $(docker ps -a -q)
@@ -27,7 +16,5 @@ docker rmi -f $(docker images -a)
 sleep 5
 
 echo "> 새 애플리케이션 배포"
-#docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
-
-#NEW_DOCKER_ID=$(docker ps -af "ancestor=*app" -q)
-#echo ">새 애플리케이션 docker id: $NEW_DOCKER_ID"
+docker load -i resell-platform-app.tar
+docker run --publish 8080:8080 resell-platform-app
